78e39460b5deced09d9bb12cb7d918e1698ed3d8
Swann HERRERA <swann@devloup.dev>
2022-06-16 22:57:09 +0200

    handle websocket events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'Pimp My Code';
+  title = "Pimp My Code";
+  ngOnInit() {
+    const socket = new WebSocket("ws://localhost:8080/ws/room_id");
+    socket.onopen = () => {
+      console.log("Connected");
+    };
+    socket.onmessage = (event) => {
+      console.log(event.data);
+    };
+    socket.onclose = () => {
+      console.log("Disconnected");
+    };
+    socket.onerror = (error) => {
+      console.error(error);
+    };
+  }
 }
