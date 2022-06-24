c6ec013c064693d4233d73d5eb1968e5fabab000
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2022-06-24 16:21:24 +0200

    [DELETE UNUSED FILES]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
-import {AuthenticationComponent} from './components/authentication/authentication.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {HttpModule} from '@angular/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HeaderComponent} from './components/header/header.component';
-import {FooterComponent} from './components/footer/footer.component';
-import {RegisterComponent} from './components/register/register.component';
 import {NgxLoadingModule} from 'ngx-loading';
 import {NgModule} from "@angular/core";
 import {ButtonModule} from "primeng/button";
-import {ForgotPasswordComponent} from './components/forgot-password/forgot-password.component';
-import {LogoutComponent} from './components/logout/logout.component';
-import {ContactSupportComponent} from './components/contact-support/contact-support.component';
 import {ToastModule} from "primeng/toast";
 import {MessageService} from "primeng/api";
 import {AutoCompleteModule} from "primeng/autocomplete";
@@ -29,13 +23,7 @@ import {DialogService} from "primeng/dynamicdialog";
 @NgModule({
     declarations: [
         AppComponent,
-        AuthenticationComponent,
         HeaderComponent,
-        FooterComponent,
-        RegisterComponent,
-        ForgotPasswordComponent,
-        LogoutComponent,
-        ContactSupportComponent,
         LiveCodingComponent,
         CommentListComponent
     ],
