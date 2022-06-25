21a8c3139ba79899a49e2d67a3fd0b61aa85a1aa
Swann HERRERA <swann@devloup.dev>
2022-06-25 18:57:09 +0200

    send update code event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,4 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "Pimp My Code";
-  ngOnInit() {
-    const socket = new WebSocket("ws://localhost:8080/ws/room_id");
-    socket.onopen = () => {
-      console.log("Connected");
-    };
-    socket.onmessage = (event) => {
-      console.log(event.data);
-    };
-    socket.onclose = () => {
-      console.log("Disconnected");
-    };
-    socket.onerror = (error) => {
-      console.error(error);
-    };
-  }
 }
